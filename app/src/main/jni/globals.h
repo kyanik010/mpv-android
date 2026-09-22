1841a31231cdb4710897c9bbeeb1090252668491
kyanik010 <kyaniky01@gmail.com>
2026-09-22 05:43:46 +0400

    refactor: make external audio instance lifecycle independent

--- app/src/main/jni/globals.h
+++ app/src/main/jni/globals.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <atomic>
+#include <pthread.h>
+#include <mpv/client.h>
 
 extern JavaVM *g_vm;
 extern mpv_handle *g_mpv;
 extern std::atomic<bool> g_event_thread_request_exit;
 
-// Independent MPV instance used exclusively for external IPTV audio.
-extern mpv_handle *g_audio_mpv;
-extern std::atomic<bool> g_audio_event_thread_request_exit;
+struct MPVInstance {
+    mpv_handle *mpv = nullptr;
+    pthread_t event_thread_id = 0;
+    std::atomic<bool> event_thread_request_exit{false};
+};
+
+extern void *audio_event_thread(void *arg);

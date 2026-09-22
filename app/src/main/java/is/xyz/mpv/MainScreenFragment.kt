001958b02be673ac7bfe90c917fed90fe8d543f0
kyanik010 <kyaniky01@gmail.com>
2026-09-22 04:06:35 +0400

    Fix URI import for Xtream player launch

--- app/src/main/java/is/xyz/mpv/MainScreenFragment.kt
+++ app/src/main/java/is/xyz/mpv/MainScreenFragment.kt
@@ -7,6 +7,7 @@ import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.net.Uri
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.LinearLayout

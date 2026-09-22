43f877d0ba53eed15794df19de244d296f50bd0c
kyanik010 <kyaniky01@gmail.com>
2026-09-22 05:25:52 +0400

    Add independent audio MPV handle globals

--- app/src/main/jni/globals.h
+++ app/src/main/jni/globals.h
@@ -5,3 +5,7 @@
 extern JavaVM *g_vm;
 extern mpv_handle *g_mpv;
 extern std::atomic<bool> g_event_thread_request_exit;
+
+// Independent MPV instance used exclusively for external IPTV audio.
+extern mpv_handle *g_audio_mpv;
+extern std::atomic<bool> g_audio_event_thread_request_exit;
